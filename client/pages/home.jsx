e81989a9dfe69681381a47a760457552a40fdfa7
leemutai <mutailee361@gmail.com>
2025-06-29 23:39:56 +0300

    creared pages and components folder

---
+++ client/pages/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page of our application.</p>
+      <p>Feel free to explore the features and functionalities we offer.</p>
+      <p>Thank you for visiting!</p>
+      <p>We hope you have a great experience.</p>
+    </div>
+  );
+};
+
+export default home;
